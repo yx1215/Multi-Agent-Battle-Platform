8393f2936181e3002459ed96d618e1a4bf7ad639
yx1215 <yx1215@nyu.edu>
2022-10-21 17:44:37 -0400

    add config file

--- backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
+++ backend/src/main/java/com/kob/backend/controller/pk/BotInfoController.java
@@ -4,12 +4,18 @@ package com.kob.backend.controller.pk;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.HashMap;
+import java.util.Map;
+
 @RestController
 @RequestMapping("/pk/")
 public class BotInfoController {
 
     @RequestMapping("getbotinfo/")
-    public String getBotInfo(){
-        return "test";
+    public Map<String, String> getBotInfo(){
+        HashMap<String, String> info = new HashMap<>();
+        info.put("name", "tiger");
+        info.put("rating", "1500");
+        return info;
     }
 }
